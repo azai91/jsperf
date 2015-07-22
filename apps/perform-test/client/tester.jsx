f756f00c64ff993980ebd4ee0e60eba9d82e8c76
formula1 <samtobia@gmail.com>
2015-07-22 12:24:19 -0700

    runs, tests and displays

--- apps/perform-test/client/tester.jsx
+++ apps/perform-test/client/tester.jsx
@@ -12,28 +12,28 @@ module.exports = React.createClass({
     console.log('starting tests');
     var _this = this;
     this.props.scripts.forEach(function(script,next){
-      context = {value:null};
+      var context = {ref_holder:{}};
       console.log(_this.props.setup);
-      vm.runInNewContext('value = function(){'+(_this.props.setup.text||"")+'\n'+script.text+'}',context);
-      console.log(context.value);
-      var test = jslitmus.test(script.title, context.value);
+      vm.runInNewContext('ref_holder.value = function(){'+(_this.props.setup.text||"")+'\n'+script.text+'}',context);
+      console.log(context.ref_holder.value.toString());
+      var test = jslitmus.test(script.title, context.ref_holder.value);
       script.test = test;
       test.script = script;
       script.state = "pending";
     });
     console.log('added tests');
     jslitmus.on('start',function(test){
+      console.log(test.script.title,' started');
       test.script.state = 'running';
-      _this.setState({});
     });
     jslitmus.on('complete', function(test) {
+      console.log(test.script.title,' finished');
       test.script.state = 'finished';
-      _this.setState({});
     });
     // Log the test results
     jslitmus.on('all_complete', function(test) {
       console.log('finished tests');
-      _this.setState({state:"finished"});
+      _this.setState({state:"finished",chart:jslitmus.getGoogleChart()});
     });
     // Run it!
     jslitmus.runAll();
@@ -41,9 +41,10 @@ module.exports = React.createClass({
   },forkTests: function(){
     window.location = this.props.repoUrl;
   },render: function(){
-    return (<div>
-      <h1>{this.state.state}</h1>
-      <button onClick={this.startTests} >Start Tests</button>
-    </div>);
+    return (<div>{[
+      <h1>{this.state.state}</h1>,
+      <button onClick={this.startTests} >Start Tests</button>,
+      <p>{this.state.chart?<img src={this.state.chart} />:"Press start tests"}</p>
+    ]}</div>);
   }
 });
